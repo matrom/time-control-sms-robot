878613b287da11549f7db75b7077204a6149c7ac
pavel.sinkevich@gmail.com <pavel.sinkevich@gmail.com@ba1eb7f1-2362-5401-5bda-5a66e41c4a46>
2012-03-23 10:27:18 +0000

    Initial project commit. Application is still a non working prototype

---
+++ trunk/TimeControlServer/TimeControlServer/SummaryView/SummaryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TimeControlServer
+{
+    class SummaryController
+    {
+        public SummaryController()
+        {
+        }
+        public object stopThreadSynch = new object();
+        public bool stopThread = false;
+        public MessageStorageModel model;
+        public SummaryView summaryView;
+        public void run()
+        {
+            bool localStop = false;
+            while (!localStop)
+            {
+                ThreadManager.newMessageByUser.WaitOne();
+                lock (model.Inbox)
+                    model.Inbox.Add(new Message());
+                lock (summaryView.Log)
+                    summaryView.Log.Add("Message received");
+                // Do something
+                lock (stopThreadSynch)
+                    localStop = stopThread;
+            }
+        }
+    }
+}
---
+++ trunk/TimeControlServer/TimeControlServer/ThreadManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TimeControlServer
+{
+    class ThreadManager : IDisposable 
+    {
+        Thread messageStorageModelThread;
+        MessageStorageModel messageStorageModel;
+        Thread summaryControllerThread;
+        SummaryController summaryController;
+        public static EventWaitHandle newMessageByUser = new AutoResetEvent(false);
+        SummaryView summaryView;
+        public ThreadManager(SummaryView view)
+        {
+            summaryView = view;
+            messageStorageModel = new MessageStorageModel();
+            messageStorageModelThread = new Thread(messageStorageModel.run);
+            messageStorageModelThread.Start();
+            summaryController = new SummaryController();
+            summaryController.model = messageStorageModel;
+            summaryController.summaryView = this.summaryView;
+            summaryControllerThread = new Thread(summaryController.run);
+            summaryControllerThread.Start();
+        }
+        public void Dispose()
+        {
+            lock (messageStorageModel.stopThreadSynch)
+                messageStorageModel.stopThread = true;
+            lock (summaryController.stopThreadSynch)
+                summaryController.stopThread = true;
+        }
+    }
+}
