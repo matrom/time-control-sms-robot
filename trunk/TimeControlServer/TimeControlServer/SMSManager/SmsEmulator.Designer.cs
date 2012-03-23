c71e6a84b524a6f4419571b4b5568cefe675a95b
pavel.sinkevich@gmail.com <pavel.sinkevich@gmail.com@ba1eb7f1-2362-5401-5bda-5a66e41c4a46>
2012-03-23 18:20:27 +0000

    SMS Emulator module created

---
+++ trunk/TimeControlServer/TimeControlServer/SMSManager/SmsEmulator.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TimeControlServer
+{
+    partial class SmsEmulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFrom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonEmulateSMSReceive = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From";
+            // 
+            // textBoxFrom
+            // 
+            this.textBoxFrom.Location = new System.Drawing.Point(184, 43);
+            this.textBoxFrom.Name = "textBoxFrom";
+            this.textBoxFrom.Size = new System.Drawing.Size(173, 20);
+            this.textBoxFrom.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Message Text";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 97);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(192, 79);
+            this.textBox1.TabIndex = 3;
+            // 
+            // buttonEmulateSMSReceive
+            // 
+            this.buttonEmulateSMSReceive.Location = new System.Drawing.Point(113, 226);
+            this.buttonEmulateSMSReceive.Name = "buttonEmulateSMSReceive";
+            this.buttonEmulateSMSReceive.Size = new System.Drawing.Size(186, 23);
+            this.buttonEmulateSMSReceive.TabIndex = 4;
+            this.buttonEmulateSMSReceive.Text = "Emulate SMS receive";
+            this.buttonEmulateSMSReceive.UseVisualStyleBackColor = true;
+            this.buttonEmulateSMSReceive.Click += new System.EventHandler(this.buttonEmulateSMSReceive_Click);
+            // 
+            // SmsEmulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 318);
+            this.Controls.Add(this.buttonEmulateSMSReceive);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFrom);
+            this.Controls.Add(this.label1);
+            this.Name = "SmsEmulator";
+            this.Text = "SmsEmulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFrom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonEmulateSMSReceive;
+    }
+}
---
+++ trunk/TimeControlServer/TimeControlServer/SMSManager/SmsEmulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TimeControlServer
+{
+    public partial class SmsEmulator : Form
+    {
+        public SmsEmulator()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEmulateSMSReceive_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
