f5c02ae0743d63b302c8204424c162a2a562e2c5
pavel.sinkevich@gmail.com <pavel.sinkevich@gmail.com@ba1eb7f1-2362-5401-5bda-5a66e41c4a46>
2012-04-10 12:40:27 +0000

    added columns [hourWarning] and [5minutesWarning] to Users table. It should be used for out of time notifications

--- trunk/Database queries/Tables/Users.sql
+++ trunk/Database queries/Tables/Users.sql
@@ -1,7 +1,7 @@
 USE [TimeControlServer]
 GO
 
-/****** Object:  Table [dbo].[Users]    Script Date: 04/06/2012 17:07:00 ******/
+/****** Object:  Table [dbo].[Users]    Script Date: 04/10/2012 15:38:25 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -15,11 +15,19 @@ CREATE TABLE [dbo].[Users](
 	[login] [nvarchar](100) NULL,
 	[passwd] [nvarchar](100) NULL,
 	[deadline] [datetime] NULL,
+	[hourWarning] [bit] NOT NULL,
+	[5minutesWarning] [bit] NOT NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[UserId] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
+GO
+
+ALTER TABLE [dbo].[Users] ADD  DEFAULT ((0)) FOR [hourWarning]
+GO
+
+ALTER TABLE [dbo].[Users] ADD  DEFAULT ((0)) FOR [5minutesWarning]
 GO
 
