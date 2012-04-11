d0bbd924b9dbd78e0ef6013247d53569bd439fb2
pavel.sinkevich <pavel.sinkevich@gmail.com>
2012-04-11 14:38:38 +0000

    Minimum time items switched from seconds to minutes; column [operation] added to transactionLog table:it will contain 'tw' value in case of time wrestling, and 'tr' value in case of common transaction

--- Database queries/Tables/transactionLog.sql
+++ Database queries/Tables/transactionLog.sql
@@ -1,7 +1,7 @@
 USE [TimeControlServer]
 GO
 
-/****** Object:  Table [dbo].[transactionLog]    Script Date: 04/06/2012 17:06:51 ******/
+/****** Object:  Table [dbo].[transactionLog]    Script Date: 04/11/2012 17:36:13 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,7 +12,9 @@ CREATE TABLE [dbo].[transactionLog](
 	[TransactionId] [int] IDENTITY(1,1) NOT NULL,
 	[Sender] [int] NOT NULL,
 	[Receiver] [int] NOT NULL,
-	[amountOfSeconds] [bigint] NOT NULL,
+	[amountOfMinutes] [bigint] NULL,
+	[operation] [nvarchar](10) NULL,
+	[TS] [datetime] NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[TransactionId] ASC
@@ -27,5 +29,8 @@ GO
 
 ALTER TABLE [dbo].[transactionLog]  WITH CHECK ADD FOREIGN KEY([Sender])
 REFERENCES [dbo].[Users] ([UserId])
+GO
+
+ALTER TABLE [dbo].[transactionLog] ADD  DEFAULT (getdate()) FOR [TS]
 GO
 
