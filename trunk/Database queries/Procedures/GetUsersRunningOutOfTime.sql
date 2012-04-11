0817d42f1a193a1115ea4e3db15ea38d22e07b0a
pavel.sinkevich@gmail.com <pavel.sinkevich@gmail.com@ba1eb7f1-2362-5401-5bda-5a66e41c4a46>
2012-04-11 14:55:42 +0000

    Added column 'minutesLeft' to GetUsersRunningOutOfTime procedure

--- trunk/Database queries/Procedures/GetUsersRunningOutOfTime.sql
+++ trunk/Database queries/Procedures/GetUsersRunningOutOfTime.sql
@@ -1,7 +1,7 @@
 USE [TimeControlServer]
 GO
 
-/****** Object:  StoredProcedure [dbo].[GetUsersRunningOutOfTime]    Script Date: 04/10/2012 17:29:26 ******/
+/****** Object:  StoredProcedure [dbo].[GetUsersRunningOutOfTime]    Script Date: 04/11/2012 17:54:48 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,10 +9,12 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
+
 CREATE procedure [dbo].[GetUsersRunningOutOfTime]
 as 
 begin
-select us.UserId, FirstName, Surname, isnull(PhoneNumber, 'Not available') as PhoneNumber, case when deadline < GETDATE() then 'OutOfTime' when DATEDIFF(MINUTE, GETDATE(), deadline) <=60 and hourWarning=0 then 'hour' when DATEDIFF(MINUTE, GETDATE(), deadline) <=5 and [5minutesWarning]=0 then '5minutes' end as warningType 
+select us.UserId, FirstName, Surname, isnull(PhoneNumber, 'Not available') as PhoneNumber, case when deadline < GETDATE() then 'OutOfTime' 
+when DATEDIFF(MINUTE, GETDATE(), deadline) <=5 and [5minutesWarning]=0 then '5minutes' when DATEDIFF(MINUTE, GETDATE(), deadline) <=60 and hourWarning=0 then 'hour' end as warningType, DATEDIFF(minute, getdate(), deadline) as minutesLeft
 into #UsersRunningOutOfTime
 from Users us
 left join PhoneNUmbers pn
@@ -23,15 +25,16 @@ or (deadline < GETDATE() and OutOfTime=0))
 and OutOfTime = 0;
 
 update Users
-set hourWarning = case when warningType='hour' then 1 else hourWarning end,
+set hourWarning = case when warningType='hour' or warningType='5minutes' then 1 else hourWarning end,
 [5minutesWarning] = case when warningType='5minutes' then 1 else [5minutesWarning] end,
 OutOfTime = case when warningType='OutOfTime' then 1 else OutOfTime end
 from Users us
 inner join #UsersRunningOutOfTime urot
 on us.UserId = urot.UserId;
 
-Select FirstName, Surname, PhoneNumber, warningType from #UsersRunningOutOfTime;
+Select FirstName, Surname, PhoneNumber, warningType, minutesLeft from #UsersRunningOutOfTime;
 end
+
 
 
 GO
