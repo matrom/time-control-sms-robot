06c97e1b70379ccc1ed16b5aa76411a6539d46db
pavel.sinkevich@gmail.com <pavel.sinkevich@gmail.com@ba1eb7f1-2362-5401-5bda-5a66e41c4a46>
2012-05-30 14:27:07 +0000

    code refactoring

--- trunk/TimeControlServer/TimeControlServer/ThreadManager.cs
+++ trunk/TimeControlServer/TimeControlServer/ThreadManager.cs
@@ -8,62 +8,21 @@ namespace TimeControlServer
 {
     class ThreadManager : IDisposable 
     {
-        Thread summaryControllerThread;
+        
         SummaryController summaryController;
         public static EventWaitHandle newMessageInInbox = new AutoResetEvent(false);
-        public static EventWaitHandle newMessageInInbox_view = new AutoResetEvent(false);
         public static EventWaitHandle newMessageByDB = new AutoResetEvent(false);
         public static EventWaitHandle newMessageBySMS = new AutoResetEvent(false);
         public static EventWaitHandle newMessageInOutbox = new AutoResetEvent(false);
-        public static EventWaitHandle newMessageInOutbox_view = new AutoResetEvent(false);
-        public static EventWaitHandle newMessageInOutbox_sms = new AutoResetEvent(false);
         SummaryView summaryView;
-        private Thread InboxListenerThread;
-        private Thread OutboxListenerThread;
-        private bool StopListeners = false;
         public ThreadManager(SummaryView view, MessageStorageModel model)
         {
-            InboxListenerThread = new Thread(InboxListener);
-            InboxListenerThread.Start();
-            OutboxListenerThread = new Thread(OutboxListener);
-            OutboxListenerThread.Start();
             summaryView = view;
-            summaryController = new SummaryController();
-            summaryController.model = model;
-            summaryController.summaryView = this.summaryView;
-            summaryControllerThread = new Thread(summaryController.run);
-            summaryControllerThread.Start();
+            summaryController = new SummaryController(model, view);
         }
         public void Dispose()
         {
-            StopListeners = true;
-            lock (summaryController.stopThreadSynch)
-                summaryController.stopThread = true;
+            
         }
-        public void InboxListener()
-        {
-            while (!StopListeners)
-            {
-                bool success = ThreadManager.newMessageInInbox.WaitOne(5000);
-                if (success)
-                {
-                    ThreadManager.newMessageInInbox_view.Set();
-                }
-            }
-        }
-        public void OutboxListener()
-        {
-            while (!StopListeners)
-            {
-                bool success = ThreadManager.newMessageInOutbox.WaitOne(5000);
-                if (success)
-                {
-                    ThreadManager.newMessageInOutbox_view.Set();
-                    ThreadManager.newMessageInOutbox_sms.Set();
-                }
-            }
-        }
-
-
     }
 }
